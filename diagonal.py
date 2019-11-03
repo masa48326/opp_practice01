32a1462f139d831f9a2b949dac23f7801a542522
shibatamasayuki <masayuki.shibata.0831@gmail.com>
2019-11-04 00:02:27 +0900

    計算式は完了
    今後
    桁数-小数点第二まで表示するように変更したい

--- diagonal.py
+++ diagonal.py
@@ -7,8 +7,22 @@ rectangle2 = Rectangle(height=3, width=3)
 rectangle2.area()  # 9.00
 rectangle2.diagonal()  # 4.24
 """
+import math
 
 
-class diagonal:
-    def __init__(self, height, weight):
-        self.area =
+class Rectangle:
+    def __init__(self, height, width):
+        self.height = height
+        self.width = width
+
+        self.area = round(self.height * self.width, 3)
+        self.diagonal = f'{math.sqrt(self.height ** 2 + self.width ** 2): .3}'
+
+
+rectangle1 = Rectangle(height=5, width=6)
+print(rectangle1.area)
+print(rectangle1.diagonal)
+
+rectangle2 = Rectangle(height=3, width=3)
+print(rectangle2.area)
+print(rectangle2.diagonal)
