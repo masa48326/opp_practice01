5721ac959a028b0c37df588a29832dcc80b1b113
shibatamasayuki <masayuki.shibata.0831@gmail.com>
2019-11-03 18:05:24 +0900

    作業途中

---
+++ diagonal.py
@@ -0,0 +1,14 @@
+"""
+rectangle1 = Rectangle(height=5, width=6)
+rectangle1.area()  # 30.00
+rectangle1.diagonal()  # 7.81
+
+rectangle2 = Rectangle(height=3, width=3)
+rectangle2.area()  # 9.00
+rectangle2.diagonal()  # 4.24
+"""
+
+
+class diagonal:
+    def __init__(self, height, weight):
+        self.area =
