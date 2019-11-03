d8ad7b34ee27abb15f8c41c5925c03fdce09a32d
shibatamasayuki <masayuki.shibata.0831@gmail.com>
2019-11-03 17:37:03 +0900

    bmi計算 作りかけ

---
+++ bmi.py
@@ -0,0 +1,34 @@
+"""
+class BMI:
+     関連しそうな属性:
+        - 身長
+        - 体重
+        - BMIという値そのもの
+     ルール:
+        - 10以上40以下<-- 常識的な範囲
+        - 表示するときは､小数点第二位まで
+            - ex: 23.678 --> 23.68
+            - ex: 23.671 --> 23.67
+     できること:
+        - ???
+"""
+
+
+# クラス名はUpperCamelCaseが普通
+class BMI:
+    def __init__(self):
+        self.value = weight / (height ** 2)
+
+        if not (10 <= self.value <= 40):
+            raise ValueErorr('BMIが正常値の範囲を超えています')
+
+    def __str__(self):
+        return f'{self.value:.2f}'
+
+
+# BMIのクラスのインスタンス化
+hibiki_bmi = BMI(height=1.80, weight=67.0)
+print(hibiki_bmi)
+
+ohira_bmi = BMI(height=1.78, weight=75.0)
+print(ohira_bmi)
